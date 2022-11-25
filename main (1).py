f88464918a6ff7a2fd7e803ca10350805bd50447
Kimbeng Faith Antia <valenciafaith716@gmail.com>
2022-11-25 16:59:07 +1400

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,6 @@
+print('Kimbeng Faith Antia Va Ndze')
+print('Friday 25th of November')
+print(''' I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!''')
+print('You can follow my progress at replit.com/@ValenciaFaith')
