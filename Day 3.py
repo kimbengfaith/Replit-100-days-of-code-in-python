e9fc1c9dcb1c8a489b15e3cfabd3e07099b5f2ba
Kimbeng Faith Antia <valenciafaith716@gmail.com>
2022-11-27 10:03:51 +1400

    Add files via upload

---
+++ Day 3.py
@@ -0,0 +1,8 @@
+name=input('name a food: ')
+plant=input('name a type of plant: ')
+method=input('name a method of cooking')
+word=input('what is a word that describes burnt food? ')
+diy=input('name a DIY item ')
+
+print('Menu')
+print(method,'Mac &',name,'with',word,plant,'on a bed of',diy)
