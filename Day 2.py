e7adc728011af71603daf08ad32abff0823f7262
Kimbeng Faith Antia <valenciafaith716@gmail.com>
2022-11-27 10:02:17 +1400

    Add files via upload

---
+++ Day 2.py
@@ -0,0 +1,8 @@
+name=input('what is your name?')
+food=input('what is your favourite food?')
+music=input('what is your favourite music?')
+place=input('where do you live?')
+print('you are' ,name)
+print('you"re probably hungry for',food)
+print('and you"re definitely getting your groove on to', music)
+print('living in the amazing',place)
