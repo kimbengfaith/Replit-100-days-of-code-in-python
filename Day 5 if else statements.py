fbe081e86a4b2279508151dcacebfa314d58bc96
Kimbeng Faith Antia <valenciafaith716@gmail.com>
2022-11-28 21:58:02 +1400

    Add files via upload

---
+++ Day 5 if else statements.py
@@ -0,0 +1,19 @@
+print("The following answers below should either be a yes or no")
+print()
+hang_around=input('Do you like hanging around? ')
+if hang_around=='no':
+  print("Then you're not Spider-man")
+else:
+  print("Hey!! you're spider man")
+print()
+gravelly=input("Do you have a gravelly voice? ")
+if gravelly=='no':
+  print("Aww, the you're not korg")
+else:
+  print("HEY!!! you're Korg")
+print()
+marvel=input("Do you often feel Marvelous? ")
+if marvel=='yes':
+  print("You're captain Marvel")
+else:
+  print("OPPS not a Captain marvel Fan")
