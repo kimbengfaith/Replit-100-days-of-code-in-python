24d57b1148b82988e471197ba9fd09b1b7da01a7
Kimbeng Faith Antia <valenciafaith716@gmail.com>
2022-11-28 21:58:37 +1400

    Add files via upload

---
+++ Day 6 login Generator.py
@@ -0,0 +1,13 @@
+# Challenge 6: Make your own login personalized login program 
+print("Hi welcome back to go.com please enter the correct information.")
+print()
+username=input("Enter your username: ")
+Password=input("Enter your password: ")
+if username=='faith' and Password=='faith12#':
+  print('welcome back',username,'we are excited to see you')
+elif username=='suzan' and Password=='suzan12#':
+  print('welcome back',username,'we are excited to see you')
+elif username=='valencia' and Password=='valencia12#':
+  print('welcome back',username,'we are excited to see you')
+else:
+  print("username or password incorrect\n please enter a correct password or username.")
