10053f6b2ca5ac8c71d90d9f46a69d84e7297058
Kimbeng Faith Antia <valenciafaith716@gmail.com>
2022-11-29 18:29:04 +1400

    Add files via upload

---
+++ Day 7 Fake fan question Generator.py
@@ -0,0 +1,22 @@
+print(""" Hello there welcome to my fake fan game.
+am a huge fan of youtube animation movies and i will love to know if you are a fan too
+ or maybe just faking it.
+lets ge started.""")
+print()
+anime=input('what is your favourite anime? ')
+if anime == 'MSA' or anime=='msa':
+  print("OHH you've got a good taste there")
+  anime_seris=input('What is the best latest series on MSA? ')
+  if anime_seris == '2 boys and 1 heart':
+    print("you are truley a fan of MSA")
+    main_character=input("What was the name of the main character? ")
+    if main_character=="agnes":
+      print("Damn!! contact me on twitter lets talk more about animation")
+    else:
+      print("nop you are wrong.")
+  else:
+    print("NOP you got it wrong")
+elif anime == 'animated story':
+  print(" okay your taste is not bad")
+else:
+  print("Yeah, that's cool and all…")
