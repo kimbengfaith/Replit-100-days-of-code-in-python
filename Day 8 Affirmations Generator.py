4761c8fc2440faf02880de36071fd9182fe68402
Kimbeng Faith Antia <valenciafaith716@gmail.com>
2022-11-30 22:36:14 +1400

    Add files via upload

---
+++ Day 8 Affirmations Generator.py
@@ -0,0 +1,51 @@
+# Affirmations generator.
+# Get your self some motivation every day and keep moving ahead.
+name=input("What's your name? ")
+day_of_week=input("What is the current day of the week?...eg Monday, Tuesday etc ")
+favourite_food=input('what is your favourite food? ')
+if day_of_week == 'Monday' or day_of_week=='monday':
+  feel=input("How do you feel today? answer with either good or bad")
+  if feel == 'good' or feel=='okay':
+    print('amazing to see that you are starting the week with a lively spirit')
+  else:
+    print('Always remember that monday is a perfect day to correct the mistakes from last week')
+elif day_of_week == 'Tuesday' or day_of_week == 'tuesday':
+  feel=input("How do you feel today? answer with either good or bad")
+  if feel == 'good' or feel=='okay':
+    print('Have a great tuesday.\n Choose to love, Choose to smile, Choose to be happy.')
+  else:
+    print("Tuesday should not be bad. it is a sign that you have survived monday.")
+elif day_of_week == 'Wednesday' or day_of_week == 'wednesday':
+  feel=input("How do you feel today? answer with either good or bad")
+  if feel == 'good' or feel=='okay': 
+    print("Focus on good thoughts and good things will happen. Happy wednesday stay positive, think positive, do positive a very good morning.")
+  else:
+    print("If you allow yourself to have a good wednesday, everything else will fall into place.")
+elif day_of_week == 'Thursday' or day_of_week == 'thursday':
+  feel=input("How do you feel today? answer with either good or bad")
+  if feel == 'good' or feel=='okay':
+    print("Today is thursday make it fab")
+  else:
+    print("There is noting more pleasurable than opening your eyes and realizing that every day good gives us a chance to try again of not looking back to love, smile and live.")
+elif day_of_week == 'Friday' or day_of_week == 'friday':
+  feel=input("How do you feel today? answer with either good or bad")
+  if feel == 'good' or feel=='okay':
+    print("If you must have the motivation, think of your paycheck on friday.")
+  else:
+    print("You made it. it's friday.")
+elif day_of_week == 'Saturday' or day_of_week == 'saturday':
+  feel=input("How do you feel today? answer with either good or bad")
+  if feel == 'good' or feel=='okay':
+    print("Imagine living life where every day is your saturdays and sundays.")
+  else:
+    print("Come its a saturday which means its time to partyyyy!!!")
+elif day_of_week == 'Sunday' or day_of_week == 'sunady':
+  feel=input("How do you feel today? answer with either good or bad")
+  if feel == 'good' or feel=='okay':
+    print("Happy you are happy. Remember each sunday is a day to fuel your soul.")
+  else:
+    print("A sunday well spent brings a week of content.")
+print()
+print("Have a nice day",name,"Remember to do your best this",day_of_week,"and dont forget to eat",favourite_food,"I know thats going to put a beautiful smile on your tongue.")
+
+  
