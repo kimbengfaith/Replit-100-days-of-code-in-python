cf1752427c6fc47677668cc490d0810755b90f5b
Kimbeng Faith Antia <valenciafaith716@gmail.com>
2022-12-18 10:25:06 +0100

    Add files via upload

---
+++ Day 34 Email spamming.py
@@ -0,0 +1,37 @@
+#Day 34 Challenge: Email spamming
+
+import os, time
+listOfEmail = []
+
+def prettyPrint():
+  os.system("clear") 
+  print("listofEmail") 
+  print()
+  for index in range(len(listOfEmail)): # len counts how many items in a list
+    print(f"{index}: {listOfEmail[index]}") 
+  time.sleep(1)
+  
+def spam():
+  os.system("clear")
+  for index in range(len(listOfEmail)):
+    print(f"Email {index+1}\n")
+    print(f"Dear {listOfEmail[index]}\nIt has come to our attention that you're missing out on the amazing Replit 100 days of code. We insist you do it right away.\n\n If you don't we will pass on your email address to every spammer we've ever encountered and also sign you up to the My Little Pony newsletter, because that's neat. We might just do that anyway.")
+    time.sleep(2)
+    os.system("clear")
+
+while True:
+  print("SPAMMER Inc.")
+  menu = input("1. Add email\n2: Remove email\n3. Show emails\n4. Get SPAMMING\n> ")
+  if menu == "1":
+    email = input("Email > ")
+    listOfEmail.append(email)
+  elif menu =="2":
+    email = input ("Email > ")
+    if email in listOfEmail:
+      listOfEmail.remove(email)
+  elif menu == "3":
+    prettyPrint() 
+  elif menu == "4":
+    spam()
+  time.sleep(1)
+  os.system("clear")
